efe64ea77eb695673abd8d24cd8c0682886e98c3
airaShrestha <ashre009@ucr.edu>
2024-02-22 11:16:12 -0600

    functionallevel

---
+++ 02-react-functionallevelcomponent/src/First.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+
+function First(){
+    return(
+        <div>
+            <h1>Welcome to Functional Level Component</h1>
+            <h2>Welcome to Functional Level Component</h2>
+            <h3>Welcome to Functional Level Component</h3>
+            <h4>Welcome to Functional Level Component</h4>
+        </div>
+    );
+}
+export default First;
