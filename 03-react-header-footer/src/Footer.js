d339d5c3d03ad83b8121c03eec989451a45ced2b
airaShrestha <ashre009@ucr.edu>
2024-02-22 12:49:24 -0600

    header-footer

---
+++ 03-react-header-footer/src/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Footer.css';
+function Footer(){
+    return(
+        <div>
+            <h5>copyright@javaexpress.com</h5>
+        </div>
+    );
+}
+export default Footer;
---
+++ 03-react-header-footer/src/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Header.css';
+function Header(){
+    return (
+        <div>
+            <h1>Java Express</h1>
+            <h5>Full stack Web Technologies</h5>
+        </div>
+    );
+}
+export default Header;
